33071eef57fbb0024645b6ebd3f97c102988508a
lp <1195083604@qq.com>
2019-05-24 14:55:24 +0800

    C:/Program Files/Git/

---
+++ learn/Day5_22/API/ArrayListApi.java
@@ -0,0 +1,22 @@
+package API;
+import java.util.ArrayList;
+import java.util.List;
+public class ArrayListApi {
+	public static void main(String[] args) {
+		/*ArrayList实现了List的接口，所以可以这样定义*/
+		 //ArrayList<int []> arrayList=new ArrayList<int []>();
+		 List<int []> arrayList=new ArrayList<int []>();
+	     int [] tmp=new int[] {1,2,3,4,5,6};
+		  arrayList.add(tmp);//默认是插入成功
+		  arrayList.remove(0);
+		  System.out.println(arrayList.get(0));
+		  for (int i = 0; i < arrayList.size(); i++) {
+			for (int j = 0; j < arrayList.get(i).length; j++) {
+				System.out.println(arrayList.get(i)[j]);
+			}
+		}
+		 
+	}
+	
+ 
+}
